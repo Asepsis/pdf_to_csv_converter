538420387c21210b7d4be16e573eaa1271414bbb
Asepsis <asepsishd@gmail.com>
2022-06-10 20:01:30 +0200

    Change env args to clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use clap::{Command, Arg};
 use pdf_extract::*;
 use regex;
-use std::env;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 struct Bahn {
@@ -28,9 +28,10 @@ struct Wettkampf {
 /// Takes a Vector from Wettkampf and saves a formated .csv file in the root folder
 /// # Arguments is a Vec<Wettkampf>
 /// # Output wk.csv
-fn convert_to_csv(wk: Vec<Wettkampf>) {
+fn convert_to_csv(wk: Vec<Wettkampf>, output_name: &str) {
     let mut csv_string = String::new();
-    csv_string.push_str("WK;Uhrzeit;Lauf;Bahn;Name;Jahrgang;Verein;Zeit;ZZ;ZZ;ZZ;ZZ;ZZ;ZZ;ZZ;ZZ;\n");
+    csv_string
+        .push_str("WK;Uhrzeit;Lauf;Bahn;Name;Jahrgang;Verein;Zeit;ZZ;ZZ;ZZ;ZZ;ZZ;ZZ;ZZ;ZZ;\n");
     for w in wk {
         // csv_string.push_str(&w.wettkampf);
         // csv_string.push_str(";");
@@ -57,33 +58,36 @@ fn convert_to_csv(wk: Vec<Wettkampf>) {
             }
         }
     }
-    std::fs::write("wk.csv", csv_string).unwrap();
-
+    std::fs::write(output_name, csv_string).unwrap();
 }
-
 
 fn main() {
 
-    let args: Vec<String> = env::args().collect();
-
-    //Extract text from PDF
-    let file_path = &args[1]; //"wk.pdf";
-    let verein_name = &args[2]; //"Verein";
+    let matches = Command::new("PDF to CSV converter")
+        .version("0.1.0")
+        .author("Asepsis")
+        .about("Converts a PDF to a CSV file")
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .value_name("FILE")
+                .takes_value(true)
+                .help("Sets the file to use")
+                )
+        .arg(Arg::new("verein").short('v').long("verein").value_name("VEREIN").takes_value(true).help("Sets the verein to use"))
+        .arg(Arg::new("output").short('o').long("output").value_name("OUTPUT").takes_value(true).help("Sets the output filename"))
+        .get_matches();
+    
+    let file_path = matches.value_of("file").unwrap();
+    let verein_name = matches.value_of("verein").unwrap_or("");
+    let output_name = matches.value_of("output").unwrap_or("wk.csv");
 
     println!("File path: {}", file_path);
     println!("Verein name: {}", verein_name);
+    println!("Output name: {}", output_name);
 
     let content = extract_text(file_path).unwrap();
-    // println!("Content: {}", content);
-
-    //Save as txt for debug purposes
-    //std::fs::write("message.txt", &content).unwrap();
-
-    //Try to solve Bahn 1 issues if Bahn 8 in is empty in the Lauf before
-    // remove all lines with (Bahn\s+)(\d+)(:\s+)$ from content
-    // let re = regex::Regex::new(r"(Bahn\s+)(\d+)(:\s+)$").unwrap();
-    // let clean_content = re.replace_all(&content, "");
-    // println!("{:#?}", clean_content);
 
     //Find all Wettkampf and there positions in the text
     let re_wk = regex::Regex::new(r"(Wettkampf\s\d+)\s-\s(\d+m\s+\S+)\s(\S.+)").unwrap();
@@ -94,7 +98,7 @@ fn main() {
             lauf_list: Vec::new(),
             byte_offset: cap_wk.get(0).unwrap().start(),
         };
-        
+
         wk_list.push(wk);
     });
 
@@ -114,8 +118,10 @@ fn main() {
 
     //Find all Bahn and there positions in the text
     let mut bahn_list: Vec<Bahn> = Vec::new();
-    let re_bahn =
-        regex::Regex::new(r"(?:\s*Bahn\s+\d+\s*)*Bahn\s+(\d+)\s+(\D+)\s+(\d+(?:/AK\s\d+)?)\s+(.+)\s+(\d+:\d+,\d+)").unwrap();
+    let re_bahn = regex::Regex::new(
+        r"(?:\s*Bahn\s+\d+\s*)*Bahn\s+(\d+)\s+(\D+)\s+(\d+(?:/AK\s\d+)?)\s+(.+)\s+(\d+:\d+,\d+)",
+    )
+    .unwrap();
     re_bahn.captures_iter(&content).for_each(|cap_bahn| {
         let bahn = Bahn {
             bahn: cap_bahn[1].to_string(),
@@ -131,7 +137,6 @@ fn main() {
         }
         // bahn_list.push(bahn);
     });
-    // println!("{:#?}", bahn_list);
 
     //Add Bahn to the appropriate Lauf
     lauf_list.iter_mut().rev().for_each(|lf| {
@@ -139,7 +144,7 @@ fn main() {
             bahn_list
                 .iter()
                 .cloned()
-                .filter(|bahn| bahn.byte_offset > lf.byte_offset)
+                .filter(|bahn| bahn.byte_offset > lf.byte_offset),
         );
         bahn_list.retain(|bahn| bahn.byte_offset < lf.byte_offset);
     });
@@ -153,7 +158,7 @@ fn main() {
             lauf_list
                 .iter()
                 .cloned()
-                .filter(|lf| lf.byte_offset > wk.byte_offset)
+                .filter(|lf| lf.byte_offset > wk.byte_offset),
         );
         lauf_list.retain(|lf| lf.byte_offset < wk.byte_offset);
     });
@@ -161,8 +166,8 @@ fn main() {
     //Remove all empty Wettkampf
     wk_list.retain(|wk| !wk.lauf_list.is_empty());
 
+    convert_to_csv(wk_list, output_name);
 
-    convert_to_csv(wk_list);
+    println!("Successfully converted PDF to CSV");
 
 }
-
